8dcf1b5223a0bbae5e70a1a072ca2ea774688bec
gayathrisunil <56356145+gayathrisunil@users.noreply.github.com>
2021-12-04 22:06:11 +0530

    Add files via upload

--- script1.py
+++ script1.py
@@ -7,6 +7,8 @@ from pyspark.ml import Pipeline
 from pyspark.ml.feature import StopWordsRemover, Word2Vec, RegexTokenizer
 from pyspark.ml.classification import LogisticRegression
 from pyspark.sql import Row
+from sklearn.linear_model import SGDClassifier
+import numpy as np
 
 sc= SparkContext("local[2]","sent")
 spark = SparkSession.builder.appName("Sentiment").getOrCreate()
@@ -20,14 +22,18 @@ def preprocessing(df):
 	stage_1 = RegexTokenizer(inputCol= 'tweet' , outputCol= 'tokens', pattern= '\\W')
 	stage_2 = StopWordsRemover(inputCol= 'tokens', outputCol= 'filtered_words')
 	stage_3 = Word2Vec(inputCol= 'filtered_words', outputCol= 'vector', vectorSize= 100)
-	model = LogisticRegression(featuresCol= 'vector', labelCol= 'score')
-	pipeline = Pipeline(stages= [stage_1, stage_2, stage_3,model])
-
+	#model = LogisticRegression(featuresCol= 'vector', labelCol= 'score')
+	pipeline = Pipeline(stages= [stage_1, stage_2, stage_3])
 	pipelineFit = pipeline.fit(df)
 	df1=pipelineFit.transform(df)
-	df1.show()
-	accuracy = df1.filter(df.score == df1.prediction).count() / df1.count()
-	print(accuracy)
+	#df1.show()
+	print(df1["vector"])
+	#batchsize=10000
+	#model = SGDClassifier(alpha=.0001, loss='log', penalty='l2', n_jobs=-1, shuffle=True)
+	#df2=model.partial_fit(np.reshape(df1["vector"], (10000,-1)), df1["score"], classes=[0,4])
+	#df2.show()
+	#accuracy = df2.filter(df2.score == df2.prediction).count() / df1.count()
+	#print(accuracy)
 	
 def temp(rdd):
 	df=spark.read.json(rdd)
