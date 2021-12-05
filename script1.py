52cca2e29b985ee40f76cc11305ec739fc3c1e98
Anisha Ghosh <73275208+Ghosh-Anisha@users.noreply.github.com>
2021-12-05 11:37:35 +0530

    something works in the model

--- script1.py
+++ script1.py
@@ -8,7 +8,9 @@ from pyspark.ml.feature import StopWordsRemover, Word2Vec, RegexTokenizer
 from pyspark.ml.classification import LogisticRegression
 from pyspark.sql import Row
 from sklearn.linear_model import SGDClassifier
+import sklearn
 import numpy as np
+import pickle
 
 sc= SparkContext("local[2]","sent")
 spark = SparkSession.builder.appName("Sentiment").getOrCreate()
@@ -27,10 +29,14 @@ def preprocessing(df):
 	pipelineFit = pipeline.fit(df)
 	df1=pipelineFit.transform(df)
 	#df1.show()
-	print(df1["vector"])
-	#batchsize=10000
-	#model = SGDClassifier(alpha=.0001, loss='log', penalty='l2', n_jobs=-1, shuffle=True)
-	#df2=model.partial_fit(np.reshape(df1["vector"], (10000,-1)), df1["score"], classes=[0,4])
+	#print(df1["vector"])
+	batchsize=10000
+	y=np.reshape(np.array(df1.select('score').collect()),(10000,1))
+	model = SGDClassifier(alpha=.0001, loss='log', penalty='l2', n_jobs=-1, shuffle=True)
+	df2=model.partial_fit(np.reshape(np.array(df1.select('vector').collect()),(10000,100)),np.reshape(np.array(df1.select('score').collect()),(10000,1)),classes=[0,4])
+	y_pred=df2.predict(np.reshape(np.array(df1.select('vector').collect()),(10000,100)))
+	accuracy=sklearn.metrics.accuracy_score(y,y_pred)	
+	print(accuracy)
 	#df2.show()
 	#accuracy = df2.filter(df2.score == df2.prediction).count() / df1.count()
 	#print(accuracy)
