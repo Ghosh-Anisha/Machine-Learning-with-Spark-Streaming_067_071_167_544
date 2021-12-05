cd8e9a446b87494483a8e21408b5910e6fd7de4d
gayathrisunil <56356145+gayathrisunil@users.noreply.github.com>
2021-12-05 20:17:45 +0530

    preprocessing works but after that it goes down hill

--- script2.py
+++ script2.py
@@ -9,10 +9,18 @@ from pyspark.ml.classification import LogisticRegression
 from pyspark.sql import Row
 from sklearn.linear_model import SGDClassifier
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import TfidfTransformer, TfidfVectorizer
+from sklearn.model_selection import train_test_split
 import sklearn
 import numpy as np
 import pickle
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+
+nltk.download('stopwords')
+nltk.download('wordnet')
 
 sc= SparkContext("local[2]","sent")
 spark = SparkSession.builder.appName("Sentiment").getOrCreate()
@@ -21,31 +29,62 @@ ssc = StreamingContext(sc, 1)
 lines = ssc.socketTextStream('localhost', 6100)
 columns=["score","tweet"]
 
+model = SGDClassifier(alpha=.0001, loss='log', penalty='l2', n_jobs=-1, shuffle=True)
+
 def preprocessing(df):
 	df_temp=np.array(df.select('tweet').collect())
-	count_vect = CountVectorizer()
-	X_train_counts = count_vect.fit_transform(df_temp.ravel())
-	
-	tf_transformer = TfidfTransformer(use_idf=False).fit(X_train_counts)
-	X_train_tf = tf_transformer.transform(X_train_counts)
-	
-	batchsize=10000
-	y=np.reshape(np.array(df.select('score').collect()),(10000,1))
-	model = SGDClassifier(alpha=.0001, loss='log', penalty='l2', n_jobs=-1, shuffle=True)
+	df_n= list()
+	stemmer = WordNetLemmatizer()
+	for sen in range(0, len(df_temp)):
+	    df_temp1 = re.sub(r'\W', ' ', str(df_temp[sen])) 
+	    df_temp1 = re.sub(r'\s+[a-zA-Z]\s+', ' ', df_temp1) 
+	    df_temp1 = re.sub(r'\^[a-zA-Z]\s+', ' ', df_temp1)  
+	    df_temp1 = re.sub(r'\s+', ' ', df_temp1, flags=re.I) 
+	    df_temp1 = re.sub(r'^b\s+', '', df_temp1)
+	    df_temp1 = df_temp1.lower()
+	    df_temp1 = df_temp1.split()
 
-	df2=model.partial_fit(X_train_tf,np.reshape(np.array(df.select('score').collect()),(10000,1)),classes=[0,4])
-	y_pred=df2.predict(X_train_tf)
-	accuracy=sklearn.metrics.accuracy_score(y,y_pred)	
+	    df_temp1 = [stemmer.lemmatize(word) for word in df_temp1]
+	    df_temp1 = ' '.join(df_temp1)
+	    df_n.append(df_temp1)
+	
+	count_vect = CountVectorizer(max_features=1500, min_df=5, max_df=0.7, stop_words=stopwords.words('english'))
+	X_train_counts = count_vect.fit_transform(df_n).toarray()
+	
+	tfidfconverter = TfidfTransformer()
+	tf_transformer= tfidfconverter.fit_transform(X_train_counts).toarray()
+	
+	tfidfconverter = TfidfVectorizer(max_features=1500, min_df=5, max_df=0.7, stop_words=stopwords.words('english'))
+	X = tfidfconverter.fit_transform(tf_transformer).toarray()
+	
+	y=np.reshape(np.array(df.select('score').collect()),(10000,1))
+	
+	X_train, X_test, y_train, y_test = train_test_split(X.reshape(-1,1), y, test_size=0.33, random_state=42)
+	
+	try:
+		model = pickle.load(open('model.pkl','rb'))
+	except:
+		pass
+	
+	df2=model.partial_fit(X_train,y_train,classes=[0,4])
+	pickle.dump(df2,open('model.pkl','wb'))
+	y_pred=df2.predict(X_test)
+	accuracy=sklearn.metrics.accuracy_score(y_test,y_pred)	
 	print(accuracy)
 
 def temp(rdd):
 	df=spark.read.json(rdd)
+	#count=0
 	for row in df.rdd.toLocalIterator():
 		conv_df = spark.createDataFrame(row,columns)
 		#conv_df.show()
 		preprocessing(conv_df)
+		#count+=1
+
 			
 lines.foreachRDD(lambda rdd : temp(rdd))
 
 ssc.start()
 ssc.awaitTermination()
+
+
