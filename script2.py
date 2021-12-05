4dc5cb061e22f71d06bd2b6cc2a91e3f1c97ee7a
Anisha Ghosh <73275208+Ghosh-Anisha@users.noreply.github.com>
2021-12-05 23:23:05 +0530

    sgd working with hashvect

--- script2.py
+++ script2.py
@@ -9,7 +9,7 @@ from pyspark.ml.classification import LogisticRegression
 from pyspark.sql import Row
 from sklearn.linear_model import SGDClassifier
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer, TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer, TfidfVectorizer, HashingVectorizer
 from sklearn.model_selection import train_test_split
 import sklearn
 import numpy as np
@@ -43,30 +43,32 @@ def preprocessing(df):
 	    df_temp1 = re.sub(r'^b\s+', '', df_temp1)
 	    df_temp1 = df_temp1.lower()
 	    df_temp1 = df_temp1.split()
-
 	    df_temp1 = [stemmer.lemmatize(word) for word in df_temp1]
 	    df_temp1 = ' '.join(df_temp1)
 	    df_n.append(df_temp1)
 	
-	count_vect = CountVectorizer(max_features=1500, min_df=5, max_df=0.7, stop_words=stopwords.words('english'))
-	X_train_counts = count_vect.fit_transform(df_n).toarray()
+	#count_vect = CountVectorizer(max_features=1500, min_df=5, max_df=0.7, stop_words=stopwords.words('english'))
+	#X_train_counts = count_vect.fit_transform(df_n)
 	
-	tfidfconverter = TfidfTransformer()
-	tf_transformer= tfidfconverter.fit_transform(X_train_counts).toarray()
+	#tfidfconverter = TfidfTransformer()
+	#tf_transformer= tfidfconverter.fit_transform(X_train_counts)
 	
-	tfidfconverter = TfidfVectorizer(max_features=1500, min_df=5, max_df=0.7, stop_words=stopwords.words('english'))
-	X = tfidfconverter.fit_transform(tf_transformer).toarray()
+	#tfidfconverter = TfidfVectorizer(max_features=1500, min_df=5, max_df=0.7, stop_words=stopwords.words('english'))
+	#X = tfidfconverter.fit_transform(df_n)
 	
+	hashvect=HashingVectorizer(stop_words=stopwords.words('english'))
+	X=hashvect.fit_transform(df_n)
 	y=np.reshape(np.array(df.select('score').collect()),(10000,1))
 	
-	X_train, X_test, y_train, y_test = train_test_split(X.reshape(-1,1), y, test_size=0.33, random_state=42)
+	X_train, X_test, y_train, y_test = train_test_split(X.reshape(10000,-1), y, test_size=0.33, random_state=42)
 	
 	try:
-		model = pickle.load(open('model.pkl','rb'))
+		model= pickle.load(open('model.pkl','rb'))
+		print(0000)
 	except:
-		pass
-	
-	df2=model.partial_fit(X_train,y_train,classes=[0,4])
+		model = SGDClassifier(alpha=.0001, loss='log', penalty='l2', n_jobs=-1, shuffle=True)
+	model1=model
+	df2=model1.partial_fit(X_train,y_train,classes=[0,4])
 	pickle.dump(df2,open('model.pkl','wb'))
 	y_pred=df2.predict(X_test)
 	accuracy=sklearn.metrics.accuracy_score(y_test,y_pred)	
@@ -86,5 +88,4 @@ lines.foreachRDD(lambda rdd : temp(rdd))
 
 ssc.start()
 ssc.awaitTermination()
-
 
