23da14e487c634b06d453283f911e59525bcfce5
Anisha Ghosh <73275208+Ghosh-Anisha@users.noreply.github.com>
2021-12-05 13:26:03 +0530

    pkl does not work

--- script1.py
+++ script1.py
@@ -8,6 +8,9 @@ from pyspark.ml.feature import StopWordsRemover, Word2Vec, RegexTokenizer
 from pyspark.ml.classification import LogisticRegression
 from pyspark.sql import Row
 from sklearn.linear_model import SGDClassifier
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.model_selection import train_test_split
 import sklearn
 import numpy as np
 import pickle
@@ -19,28 +22,39 @@ ssc = StreamingContext(sc, 1)
 lines = ssc.socketTextStream('localhost', 6100)
 
 columns=["score","tweet"]
-
+model = SGDClassifier(alpha=.0001, loss='log', penalty='l2', n_jobs=-1, shuffle=True)
 def preprocessing(df):
+	df_temp=np.array(df.select('tweet').collect())
+	
+	"""
 	stage_1 = RegexTokenizer(inputCol= 'tweet' , outputCol= 'tokens', pattern= '\\W')
 	stage_2 = StopWordsRemover(inputCol= 'tokens', outputCol= 'filtered_words')
 	stage_3 = Word2Vec(inputCol= 'filtered_words', outputCol= 'vector', vectorSize= 100)
-	#model = LogisticRegression(featuresCol= 'vector', labelCol= 'score')
+	model = LogisticRegression(featuresCol= 'vector', labelCol= 'score')
 	pipeline = Pipeline(stages= [stage_1, stage_2, stage_3])
 	pipelineFit = pipeline.fit(df)
 	df1=pipelineFit.transform(df)
-	#df1.show()
-	#print(df1["vector"])
+	df1.show()
+	print(df1["vector"])
+	"""
+	count_vect = CountVectorizer()
+	X_train_counts = count_vect.fit_transform(df_temp.ravel())
+	tf_transformer = TfidfTransformer(use_idf=False).fit(X_train_counts)
+	X_train_tf = tf_transformer.transform(X_train_counts)
+	y=np.reshape(np.array(df.select('score').collect()),(10000,1))
 	batchsize=10000
-	y=np.reshape(np.array(df1.select('score').collect()),(10000,1))
-	model = SGDClassifier(alpha=.0001, loss='log', penalty='l2', n_jobs=-1, shuffle=True)
-	df2=model.partial_fit(np.reshape(np.array(df1.select('vector').collect()),(10000,100)),np.reshape(np.array(df1.select('score').collect()),(10000,1)),classes=[0,4])
-	y_pred=df2.predict(np.reshape(np.array(df1.select('vector').collect()),(10000,100)))
-	accuracy=sklearn.metrics.accuracy_score(y,y_pred)	
-	print(accuracy)
-	#df2.show()
-	#accuracy = df2.filter(df2.score == df2.prediction).count() / df1.count()
-	#print(accuracy)
 	
+	X_train, X_test, y_train, y_test = train_test_split(X_train_tf, y, test_size=0.33, random_state=42)
+	#df2=model.partial_fit(np.reshape(np.array(df1.select('vector').collect()),(10000,100)),np.reshape(np.array(df1.select('score').collect()),(10000,1)),classes=[0,4])
+
+
+	df2=model.partial_fit(X_train,y_train,classes=[0,4])
+	pickle.dump(df2,open('model.pkl','wb'))
+	model = pickle.load(open('model.pkl','rb'))
+	y_pred=df2.predict(X_test)
+	accuracy=sklearn.metrics.accuracy_score(y_test,y_pred)	
+	print(accuracy)
+
 def temp(rdd):
 	df=spark.read.json(rdd)
 	for row in df.rdd.toLocalIterator():
