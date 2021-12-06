988618c40a0ac7350f68aa695931e03874e8cd0c
Anupama Nhavalore <55904909+anupama-nhavalore@users.noreply.github.com>
2021-12-06 15:35:22 +0530

    naive bayes workingg

--- script_NB.py
+++ script_NB.py
@@ -10,7 +10,6 @@ from pyspark.sql import Row
 from sklearn.linear_model import SGDClassifier
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer, TfidfVectorizer, HashingVectorizer
-from sklearn.preprocessing import FunctionTransformer
 from sklearn.model_selection import train_test_split
 import sklearn
 import numpy as np
@@ -18,14 +17,10 @@ import pickle
 import re
 import nltk
 from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer, PorterStemmer
-from nltk.tokenize import word_tokenize
-from sklearn.naive_bayes import GaussianNB
-
-
-import warnings
-
-ps= PorterStemmer()
+from nltk.stem import WordNetLemmatizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.preprocessing import MaxAbsScaler
+from sklearn.decomposition import NMF
 
 nltk.download('stopwords')
 nltk.download('wordnet')
@@ -38,7 +33,7 @@ ssc = StreamingContext(sc, 1)
 lines = ssc.socketTextStream('localhost', 6100)
 columns=["score","tweet"]
 
-clf = GaussianNB()
+clf = MultinomialNB()
 def preprocessing(df):
 	df_temp=np.array(df.select('tweet').collect())
 	df_n= list()
@@ -51,38 +46,37 @@ def preprocessing(df):
 	    df_temp1 = re.sub(r'^b\s+', '', df_temp1)
 	    df_temp1 = df_temp1.lower()
 	    df_temp1 = df_temp1.split()
-	    
-	    #df_temp1 = [word_tokenize(word) for word in df_temp1]
-	    #df_temp1 = [ps.stem(word) for word in df_temp1]
 	    df_temp1 = [stemmer.lemmatize(word) for word in df_temp1]
-	    
 	    df_temp1 = ' '.join(df_temp1)
 	    df_n.append(df_temp1)
 	
 	#count_vect = CountVectorizer(max_features=1500, min_df=5, max_df=0.7, stop_words=stopwords.words('english'))
 	#X_train_counts = count_vect.fit_transform(df_n)
 	
-	hashvect=HashingVectorizer(stop_words=stopwords.words('english'))
-	X=hashvect.fit_transform(df_n)
+	#tfidfconverter = TfidfTransformer()
+	#tf_transformer= tfidfconverter.fit_transform(X_train_counts)
+	
+	#tfidfconverter = TfidfVectorizer(max_features=1500, min_df=5, max_df=0.7, stop_words=stopwords.words('english'))
+	#X = tfidfconverter.fit_transform(df_n)
+	
+	X=np.array(df_n)
+	hashvect=HashingVectorizer(stop_words=stopwords.words('english'),alternate_sign=False)
+	X_f=hashvect.fit_transform(X)
+
 	y=np.reshape(np.array(df.select('score').collect()),(10000,1))
 	
-	X_train, X_test, y_train, y_test = train_test_split(X.reshape(10000,-1), y, test_size=0.33, random_state=42)
+	X_train, X_test, y_train, y_test = train_test_split(X_f.reshape(10000,-1), y.reshape(-1,1), test_size=0.33, random_state=42)
 	
 	try:
-		#model= pickle.load(open('model.pkl','rb'))
-		clf= pickle.load(open('model1.pkl','rb'))
+		clf= pickle.load(open('model.pkl','rb'))
+		print(0000)
 	except:
-		#model = SGDClassifier(alpha=.0001, loss='log', penalty='l2', n_jobs=-1, shuffle=True)
-		clf = GaussianNB()
-	
-	clf1=clf
-	#df2=model1.partial_fit(X_train,y_train,classes=[0,4])
-	func=FunctionTransformer(lambda x: x.todense(), accept_sparse=True)
-	X_train = func.fit_transform(X_train)
-	df3=clf1.partial_fit(X_train,y_train,classes=[0,4])
-	#pickle.dump(df2,open('model.pkl','wb'))
-	pickle.dump(df3,open('model1.pkl','wb'))
-	y_pred=df3.predict(X_test)
+		clf = MultinomialNB()
+		
+	model1=clf
+	df2=model1.partial_fit(X_train,y_train,classes=[0,4])
+	pickle.dump(df2,open('model.pkl','wb'))
+	y_pred=df2.predict(X_test)
 	accuracy=sklearn.metrics.accuracy_score(y_test,y_pred)	
 	print(accuracy)
 
