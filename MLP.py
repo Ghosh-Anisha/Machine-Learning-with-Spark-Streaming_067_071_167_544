fe222e6084a70b3d29f4f9bda5c65cab96b2ada5
Ghosh-Anisha <73275208+Ghosh-Anisha@users.noreply.github.com>
2021-12-06 22:38:05 +0530

    FINAL

--- MLP.py
+++ MLP.py
@@ -50,14 +50,7 @@ def preprocessing(df):
 	    df_temp1 = ' '.join(df_temp1)
 	    df_n.append(df_temp1)
 	
-	#count_vect = CountVectorizer(max_features=1500, min_df=5, max_df=0.7, stop_words=stopwords.words('english'))
-	#X_train_counts = count_vect.fit_transform(df_n)
 	
-	#tfidfconverter = TfidfTransformer()
-	#tf_transformer= tfidfconverter.fit_transform(X_train_counts)
-	
-	#tfidfconverter = TfidfVectorizer(max_features=1500, min_df=5, max_df=0.7, stop_words=stopwords.words('english'))
-	#X = tfidfconverter.fit_transform(df_n)
 	
 	hashvect=HashingVectorizer(stop_words=stopwords.words('english'))
 	X=hashvect.fit_transform(df_n)
